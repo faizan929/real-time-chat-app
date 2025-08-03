51efd318184af8fde7a983edc32d305c1ad78442
Faizan Sheikh <faizanjavid312@gmail.com>
2025-08-03 18:18:39 +0000

    Added multiple client logic

--- server/server.py
+++ server/server.py
@@ -15,20 +15,32 @@ async def receive_messages(websocket):
 
     except websockets.ConnectionClosed:
         print("Client disconnected..")
+        connected_users.remove(websocket)
     
 async def send_messages(websocket):
     try:
         while True:
             msg = await asyncio.to_thread(input, " Server: ")
-            await websocket.send(msg)
+            for user in connected_users:
+                await user.send(f"[Broadcast] {msg}")
+            #await websocket.send(msg)
     except websockets.ConnectionClosed:
         print("Cant send, client disconnected")
+        connected_users.remove(websocket)
 
 async def handler(websocket):
-    await asyncio.gather(
-        receive_messages(websocket),
-        send_messages(websocket)
+    connected_users.add(websocket)
+    print("New client connected")
+    try:    
+        await asyncio.gather(
+            receive_messages(websocket),
+            send_messages(websocket)
     )
+    except:
+        pass
+    finally:
+        if websocket in connected_users:
+            connected_users.remove(websocket)
 
 async def main():
     async with websockets.serve(handler, "localhost", 9002):
