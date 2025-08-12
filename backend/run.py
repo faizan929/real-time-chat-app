af0ec68a17811b410055187e708511211e053fa1
Faizan Sheikh <faizanjavid312@gmail.com>
2025-08-12 21:32:11 +0530

    Changed run.py

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,15 @@
 
 import os
 import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello from FastAPI on Railway!"}
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host = "0.0.0.0", port = port)
+    uvicorn.run("run:app", host="0.0.0.0", port=port)
