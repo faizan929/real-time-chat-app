6599f0004f79d0b4a12cba7ab9c49d996e8ed1fb
Faizan Sheikh <faizanjavid312@gmail.com>
2025-08-12 21:51:11 +0530

    edited database.py

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,11 @@
 
-
+import os
 from  sqlalchemy import create_engine 
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base 
 
 
-DATABASE_URL = "postgresql://postgres:faizan123@localhost/whatsapp_clone"
+DATABASE_URL = os.environ.get("DATABASE_URL")
 engine = create_engine(DATABASE_URL) 
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 Base = declarative_base()
